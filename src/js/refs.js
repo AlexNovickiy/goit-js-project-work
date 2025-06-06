8eca133c9d18c04f391aac2e9013c85a054b5b13
AlexNovickiy <novickiisasha78@gmail.com>
2025-06-06 14:30:23 +0300

    Add header markup and  styles, mobile menu functionality

---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export const refs = {
+  burgerBtnOpenMenu: document.querySelector('.icon-burger'),
+  burgerBtnCloseMenu: document.querySelector('.icon-close'),
+  headerMenuContainer: document.querySelector('.header-menu-container'),
+};
