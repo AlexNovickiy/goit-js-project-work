e86ee6553e69d7001d51d686cffb8c24013704da
AlexNovickiy <novickiisasha78@gmail.com>
2025-06-08 16:25:42 +0300

    Update icon usage in renderArtistCards function to use SVG sprite reference

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,3 +1,5 @@
+import spriteSVGArrowRightModal from '../img/sprite.svg?url';
+
 export function renderArtistCards(artists, container) {
   const markup = artists
     .map(artist => {
@@ -19,7 +21,7 @@ export function renderArtistCards(artists, container) {
             <h3 class="artist-card-name">${artist.strArtist}</h3>
             <p class="artist-card-description">${bio}</p>
             <button id="learn-more-btn" class="artist-card-link" data-artist-id="${artist._id}">Learn More <svg class="icon-lernmore" width="24" height="24">
-      <use href="./public/caret-right.svg"></use>
+      <use href="${spriteSVGArrowRightModal}#icon-caret-arrow-artist-right"></use>
     </svg></button>
           </div>
         </li>
